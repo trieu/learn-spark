84c144058728e2e480a8b0351a2d4932f0579bef
trieu <tantrieuf31@gmail.com>
2017-02-02 16:05:27 +0700

    test code

---
+++ data-analytics-spark/src/main/scala/learning/scala/BasicOOP.scala
@@ -0,0 +1,20 @@
+package learning.scala
+
+object BasicOOP {
+
+  def main(args: Array[String]): Unit = {
+    for {
+      x <- Seq(1, 2, 2.7, "one", "two", 'four) // 
+    } {
+      val str = x match { // 
+        case 1          => "int 1" // 
+        case i: Int     => "other int: " + i // 
+        case d: Double  => "a double: " + x // 
+        case "one"      => "string one" // 
+        case s: String  => "other string: " + s // 
+        case unexpected => "unexpected value: " + unexpected // 
+      }
+      println(str) // 
+    }
+  }
+}
